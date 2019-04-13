3c7bfa551521e7fc213637a36d9cb03c4885fa7f
zj-cs2103 <zj.cs2103@gmail.com>
2019-04-13 21:52:09 +0800

    tests: add tests for UpdateCapacityCommandParser

---
+++ src/test/java/seedu/address/logic/parser/booking/UpdateCapacityCommandParserTest.java
@@ -0,0 +1,38 @@
+package seedu.address.logic.parser.booking;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST;
+
+import org.junit.Test;
+
+import seedu.address.logic.commands.booking.UpdateCapacityCommand;
+import seedu.address.model.booking.Capacity;
+
+/**
+ * Checks if capacity is properly parsed by the {@code UpdateCapacityCommandParser}
+ */
+public class UpdateCapacityCommandParserTest {
+    private UpdateCapacityCommandParser parser = new UpdateCapacityCommandParser();
+
+    @Test
+    public void parse_validArgs_returnsUpdateCapacityCommand() {
+
+        Capacity capacity = new Capacity(50);
+        assertParseSuccess(parser, "50", new UpdateCapacityCommand(capacity));
+
+        capacity = new Capacity(10000);
+        assertParseSuccess(parser, "10000", new UpdateCapacityCommand(capacity));
+
+        capacity = new Capacity(1);
+        assertParseSuccess(parser, "1", new UpdateCapacityCommand(capacity));
+    }
+
+    @Test
+    public void parse_invalidArgs_throwsParseException() {
+        assertParseFailure(parser, "a", Capacity.MESSAGE_CONSTRAINTS);
+        assertParseFailure(parser, "10001", Capacity.MESSAGE_CONSTRAINTS);
+        assertParseFailure(parser, "0", Capacity.MESSAGE_CONSTRAINTS);
+    }
+}
